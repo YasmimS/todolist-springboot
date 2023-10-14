1c8579c1405e8c03f15c3a47947f821d8397388c
YasmimS <yasmim.hpforever@gmail.com>
2023-10-14 19:31:51 -0300

    Dockerfile deploy

---
+++ src/test/java/br/com/yasmimsancha/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yasmimsancha.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
